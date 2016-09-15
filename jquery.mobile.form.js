b975be068b30003468c4c2f846b1743939a7f453
Girts Silis <girts.silis@gmail.com>
2016-09-15 09:35:46 -0500

    Working example of onSubmit tabbing
    Hitting enter on one of the fields will cause the form to look for the
    next tabbable field.

--- jquery.mobile.form.js
+++ jquery.mobile.form.js
@@ -3,7 +3,10 @@
 
   function MobileForm($element, options) {
     this.$element = $element;
+    this.tabableInputs = generateTabableSelector();
     this.onSubmit = $.proxy(this.onSubmit, this);
+    this.onFocus = $.proxy(this.onFocus, this);
+    this.focusedFields = {};
     this.attachListeners();
   }
 
@@ -12,20 +15,47 @@
 
     attachListeners: function() {
       this.$element.on('submit', this.onSubmit);
+      this.$element.on('focus', this.tabableInputs, this.onFocus);
     },
 
     destroy: function() {
       this.removeListeners();
       this.$element = null;
       this.onSubmit = null;
+      this.onFocus = null;
+      this.focusedFields = null;
+    },
+
+    findTabableFieldAfter: function(field) {
+      var $formFields = this.$element.find(this.tabableInputs);
+      var fieldIndex = $formFields.index(field);
+      var focusedFields = this.focusedFields;
+      var availableFields = $formFields
+        .slice(fieldIndex)
+        .filter(function(index, field) {
+          return !focusedFields[field.name];
+        });
+
+      return availableFields[0];
+    },
+
+    onFocus: function(event) {
+      var field = event.originalEvent.target || event.originalEvent.currentTarget;
+      this.focusedFields[field.name] = true;
     },
 
     onSubmit: function() {
-      return false;
+      var nextTabableField = this.findTabableFieldAfter(document.activeElement);
+
+      if (nextTabableField) {
+        nextTabableField.focus();
+        return false;
+      }
     },
 
     removeListeners: function() {
       this.$element.off('submit', this.onSubmit);
+      this.$element.off('focus', this.tabableInputs, this.onFocus);
     }
   };
 
@@ -47,6 +77,31 @@
       plugin.destroy();
       $element.data(DATA_KEY, null);
     }
+  }
+
+  function generateTabableSelector() {
+    var tabables = [
+      'color',
+      'date',
+      'datetime',
+      'email',
+      'month',
+      'number',
+      'password',
+      'search',
+      'submit',
+      'tel',
+      'text',
+      'time',
+      'url',
+      'week'
+    ].map(function(type) {
+      return 'input[type=' + type + ']';
+    });
+
+    tabables.push('select', 'textarea');
+
+    return tabables.join(',');
   }
 
   $.fn.extend({
